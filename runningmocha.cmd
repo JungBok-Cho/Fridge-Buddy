e672cca93d2525cf18fbfbbc68b109f12f1ebaac
Ruifeng Wang <rwang@seattleu.edu>
2021-06-06 01:03:33 -0700

    Update mocha running command

--- runningmocha.cmd
+++ runningmocha.cmd
@@ -1,2 +1,3 @@
-node node_modules\mocha\bin\_mocha --reporter spec test/getMultipleTest.js
-node node_modules\mocha\bin\_mocha --reporter spec test/getSingleTest.js
+node node_modules\mocha\bin\_mocha --reporter spec test/testAzureGetMultiple.js
+node node_modules\mocha\bin\_mocha --reporter spec test/testAzureGetSingle.js
+node node_modules\mocha\bin\_mocha --reporter spec test/testAzurePost.js
